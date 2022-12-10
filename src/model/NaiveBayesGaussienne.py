19afe908e4bd3e5f6099c11d71cc5ffad9e96d99
Nouhayla Benhammou <77208565+NouhaylaBenhammou555@users.noreply.github.com>
2022-12-10 21:50:24 +0100

    Update NaiveBayesGaussienne.py

--- src/model/NaiveBayesGaussienne.py
+++ src/model/NaiveBayesGaussienne.py
@@ -4,9 +4,9 @@ from model.Classifier import Classifier
 
 class NaiveBayesGaussienne(Classifier):
 
-    def _init_(self, Train, Test, Labels, Ids_Test, Classes):
+    def __init__(self, Train, Test, Labels, Ids_Test, Classes):
 
-        super(NaiveBayesGaussienne, self)._init_(Train, Test, Labels, Ids_Test, Classes)
-        self.name = GaussianNB._name_
+        super(NaiveBayesGaussienne, self).__init__(Train, Test, Labels, Ids_Test, Classes)
+        self.name = GaussianNB.__name__
         self._classifier = GaussianNB()
         self._param_grid = {'var_smoothing': [0.01,0.001, 0.0001,0.00001,0.02, 0.002, 0.0002,0.00002]}
