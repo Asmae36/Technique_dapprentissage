671a6d37694cbbf60f35e5326183dcdbaad6ffb3
Nouhayla Benhammou <77208565+NouhaylaBenhammou555@users.noreply.github.com>
2022-12-10 21:51:41 +0100

    Update KNN.py

--- src/model/KNN.py
+++ src/model/KNN.py
@@ -5,8 +5,8 @@ from sklearn.neighbors import KNeighborsClassifier
 
 class KNN(Classifier):
 
-    def _init_(self, Train, Test, Labels, Ids_Test, Classes):
-        super(KNN, self)._init_(Train, Test, Labels, Ids_Test, Classes)
-        self.name = KNeighborsClassifier._name_
+    def __init__(self, Train, Test, Labels, Ids_Test, Classes):
+        super(KNN, self).__init__(Train, Test, Labels, Ids_Test, Classes)
+        self.name = KNeighborsClassifier.__name__
         self._classifier = KNeighborsClassifier()
         self._param_grid = {'n_neighbors': [1, 2, 3, 4, 5],'weights': ['distance','uniform'],'algorithm': ['ball_tree','brute' ,'auto','kd_tree'],'leaf_size': [10, 20, 30, 40, 50],'p': [1, 2]}
